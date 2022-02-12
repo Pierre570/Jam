56362abdf48bffca95ba0198a1ec812c581d075e
Pierre AILLET <pierre.aillet@epitech.eu>
2022-02-12 18:34:55 +0100

    [ADD]: parse csv file

--- Game Jam project/Assets/Script/reader.cs
+++ Game Jam project/Assets/Script/reader.cs
@@ -11,23 +11,30 @@ public class reader : MonoBehaviour
         ReadCsvFile();
     }
 
-    void ReadCsvFile()
+    public static List<Dictionary<string, object>> ReadCsvFile()
     {
         StreamReader strReader = new StreamReader("Assets/informationCharacter.csv");
+        var list = new List<Dictionary<string, object>>();
         bool endOffFile = false;
+        var id = strReader.ReadLine().Split(';');
         while (!endOffFile)
         {
+            var entry = new Dictionary<string, object>();
             string data_string = strReader.ReadLine();
             if(data_string == null)
             {
                 endOffFile = true;
                 break;
             }
-            var data_values = data_string.Split(',');
+            var data_values = data_string.Split(';');
+            Debug.Log(data_values);
             for (int i = 0; i < data_values.Length;i++)
             {
-                Debug.Log("Value:"+i.ToString() + " "+data_values[i].ToString());
+                entry[id[i]] = data_values[i];
+                Debug.Log("Value:"+id[i] + " "+ entry[id[i]].ToString());
             }
+            list.Add(entry);
         }
+        return (list);
     }
 }
